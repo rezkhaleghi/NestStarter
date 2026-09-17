016b973c7612094da79d2d1b2b82e3f66adc3083
rezkhaleghi <rezaxkhaleghi@gmail.com>
2026-09-17 03:49:02 +0330

    delete migratons and synch true the db

--- src/infrastructure/config/data-source.ts
+++ src/infrastructure/config/data-source.ts
@@ -40,7 +40,7 @@ export default new DataSource({
     TicketCategoryOrmEntity,
   ],
   migrations: [__dirname + "/../database/migrations/*.{js,ts}"],
-  synchronize: false, // Set to false in production to avoid data loss
+  synchronize: true, // Set to false in production to avoid data loss
 });
 
 //                     Database
